b8f80f6848318778f070ad775c16301b79545754
Priyanka Dutta <pinks@keylime.soe.ucsc.edu>
2022-02-22 12:09:13 -0800

    Updated code to print time

--- BlackJack_C++/Testrun.cpp
+++ BlackJack_C++/Testrun.cpp
@@ -7,7 +7,11 @@
 int main()
 {
 	Tests t;
-    t.test_dealt_two_cards();
-    t.run_test();
+	auto start = std::chrono::system_clock::now();
+	t.test_dealt_two_cards();
+	t.run_test();
+	auto end = std::chrono::system_clock::now();	
+	std::chrono::duration<double, std::milli> fp_ms = end - start;
+	std::cout << "Time elapsed for the whole test " << fp_ms.count() << " ms " ;
 	return 0;
 }
